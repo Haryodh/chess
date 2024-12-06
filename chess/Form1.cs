51595d1e72dde724dcedb86dda46cb55adb35d1b
Haryodh Singh <233260@student.wqe.ac.uk>
2024-12-06 12:52:40 +0000

    yay!

---
+++ chess/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chess
+{
+
+    public partial class Form1 : Form
+    {
+        //--------------------------------------------------------------------------------
+        Panel panel = new Panel();
+        //--------------------------------------------------------------------------------
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int screenwidth = this.Width;
+            int screenheight = this.Height;
+            this.Resize += resize;
+            this.MinimumSize = new Size(480,270);
+
+
+            board[] boards = new board[10];
+            int currentboard = 0;
+            boards[0] = new board(1);
+
+
+            this.Controls.Add(panel);
+            displayBoard();
+        }
+
+        private void resize(object sender, EventArgs e)
+        {
+            displayBoard();
+        }
+
+        private void displayBoard()
+        {
+            int scale = 66;
+
+            int screenwidth = this.Width;
+            int screenheight = this.Height;
+            
+            int scaledwidth = (this.Width * scale / 100);
+            int scaledheight = (this.Height * scale / 100);
+
+            if (scaledwidth > scaledheight)
+            {
+                scaledwidth = scaledheight;
+            }
+            else
+            {
+                scaledheight = scaledwidth;
+            }
+
+            panel.Location = new Point(screenwidth / 2 - scaledwidth / 2, screenheight / 2 - scaledheight / 2);
+            panel.Size = new Size(scaledwidth, scaledheight);
+            panel.BackColor = Color.Black;
+        }
+    }
+}
---
+++ chess/board.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chess
+{
+    internal class board
+    {
+        private int[,] grid;
+
+        public board(int setup)
+        {
+            setboard(setup);
+        }
+
+        public int[,] getboard() { return grid; }
+
+        public int setboard(int setup)
+        {
+            /*
+             * #0 = Base Game
+             * 
+             * 
+            */
+
+            /*
+             * 01 = White Pawn
+             * 02 = White King
+             * 03 = White Queen
+             * 04 = White Knight
+             * 05 = White Rook
+             * 06 = White Bishop
+             *
+             * 11 = Black Pawn
+             * 12 = Black King
+             * 13 = Black Queen
+             * 14 = Black Knight
+             * 15 = Black Rook
+             * 16 = Black Bishop
+            */
+
+
+            int[,] base_game = { { 05,06,04,02,03,04,06,02 },
+                                 { 01,01,01,01,01,01,01,01 },
+                                 { 00,00,00,00,00,00,00,00 },
+                                 { 00,00,00,00,00,00,00,00 },
+                                 { 00,00,00,00,00,00,00,00 },
+                                 { 00,00,00,00,00,00,00,00 },
+                                 { 11,11,11,11,11,11,11,11 },
+                                 { 15,16,14,12,13,14,16,12 } };
+
+
+            switch (setup)
+            {
+                case 0:
+                    grid = base_game;
+                    return 1;
+                default:
+                    grid = base_game;
+                    return -1;
+            }
+
+        }
+    }
+
+
+}
